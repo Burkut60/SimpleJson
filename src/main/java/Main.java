0503c83a4d6913bb9fef9066e5413192485a4cd0
Burkut60 <=kamilla2016@111>
2021-02-10 23:32:20 +0300

    First used Json!!!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,11 @@
+
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.Scanner;
 
 public class Main {
-
-
     public static void main(String[] args) throws IOException, ParseException {
 
-        System.out.println("Выберите интересующую вас информацию: \n A - JsonData \n B - advertisment \n" +
-                "C - news ");
-        Scanner sc = new Scanner(System.in);
-        String enumeration = sc.nextLine();
-
-        switch (enumeration) {
-            case "A": {
-                String dbUrl = "https://my-json-server.typicode.com/Shiplayer/ExampleJsonData/db";
-                URL myUrl = ParserJson.createUrl(dbUrl);
-                String parserReader = ParserJson.parsUrl(myUrl);
-                ParserJson.parseObject(parserReader);
-
-                break;
-            }
-            case "B": {
-                String advertUrl =
-                        "https://my-json-server.typicode.com/Shiplayer/ExampleJsonData/advertisment";
-                URL myUrl = ParserJson.createUrl(advertUrl);
-                String parserReader = ParserJson.parsUrl(myUrl);
-                ParserJson.parseObject(parserReader);
-                break;
-            }
-            case "C": {
-                String newsUrl = "https://my-json-server.typicode.com/Shiplayer/ExampleJsonData/news";
-                URL myUrl = ParserJson.createUrl(newsUrl);
-                String parserReader = ParserJson.parsUrl(myUrl);
-                ParserJson.parseObject(parserReader);
-                break;
-            }
-        }
-
+        Menu.menu();
     }
 }
--- src/main/java/ParserJson.java
+++ src/main/java/ParserJson.java
@@ -3,7 +3,6 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.json.simple.parser.ParseException;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,30 +11,39 @@ import java.net.URL;
 
 public class ParserJson {
 
-
     public static URL createUrl(String url) throws MalformedURLException {
+        if (url != null){
+        }
         return new URL(url);
     }
 
     public static String parsUrl(URL url) throws IOException {
         StringBuilder stringBuilder = new StringBuilder();
-
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
         String line;
         while ((line = bufferedReader.readLine()) != null) {
             stringBuilder.append(line);
         }
+
         return stringBuilder.toString();
     }
 
-    public static void parseObject (String result) throws ParseException {
+    public static void parseObject(String result) throws ParseException {
         JSONObject job = (JSONObject) JSONValue.parseWithException(result);
-        System.out.println("Название" + job.get("news"));//  не могу вывести названия массива news
+        System.out.println("news" + job.get("news"));//  не могу вывести названия массива news
 
         JSONArray jsonArray = (JSONArray) job.get("news");
-        for( int i=0; i<jsonArray.size(); i++ ){
+        for (int i = 0; i < jsonArray.size(); i++) {
             System.out.println("array is " + jsonArray.get(i));
 
+        }
+    }
+
+    public static void parserArray(String result) throws ParseException {
+        JSONArray jsonArray = (JSONArray) JSONValue.parseWithException(result);
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+            System.out.println("array is " + jsonArray.get(i));
         }
     }
 }
